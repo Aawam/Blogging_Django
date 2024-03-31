411204b1f43bcba1f77c819b1d44683fb8130678
Aawam <dawamid2@gmail.com>
2024-04-01 02:18:50 +0700

    commit

--- base_blog/authors/models.py
+++ base_blog/authors/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Authors(models.Model):
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=225)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=225)
+    email = models.EmailField(null=True)
+    password = models.TextField(null=True)
--- base_blog/authors/views.py
+++ base_blog/authors/views.py
@@ -1,8 +1,13 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Authors
 
 # Create your views here.
 def authors(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    all_authors = Authors.objects.all().values()
+    template = loader.get_template('all_authors.html')
+    context = {
+        'All_Authors': all_authors
+    }
+    return HttpResponse(template.render(context,request))
