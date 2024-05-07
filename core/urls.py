7c2d68d82788d4423f8411ef33ab39ac79f8e3f1
Aawam <dawamid2@gmail.com>
2024-05-07 19:54:32 +0700

    Commit & Pull

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('blog.urls')),
-    path('users/', include('users.urls')),
+
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('users/', include('users.urls')),
+
+
 ]
 
